1a162f51074de25692b77e4320399700455d1b61
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-10 08:27:05 -0400

    add a documentation

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using Documenter
+
+dir = @__DIR__() * "/.."
+cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force = true)
+
+makedocs(sitename = "COLPRAC",
+         authors = "Chris Rackauckas",
+         modules = [],
+         clean = true, doctest = false,
+         strict = [
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
+         format = Documenter.HTML(analytics = "UA-90474609-3",
+                                  assets = ["assets/favicon.ico"],
+                                  canonical = "https://colprac.sciml.ai/stable/"),
+         pages = [
+             "ColPrac: Contributor's Guide on Collaborative Practices for Community Packages" => "index.md",
+         ])
+
+deploydocs(;
+           repo = "github.com/SciML/COLPRAC")
