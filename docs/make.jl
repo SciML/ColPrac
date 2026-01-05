b89f7a51ce7a79de456d99807f755343141174b3
Chris Rackauckas - Beep Boop Edition <admin@chrisrackauckas.com>
2026-01-05 07:14:18 -0500

    Replace deprecated strict with warnonly in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,13 +8,15 @@ makedocs(
     authors = "Chris Rackauckas",
     modules = Module[],
     clean = true, doctest = false,
-    strict = [
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+    warnonly = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :eval_block,
+        :footnote,
+        :meta_block,
+        :missing_docs,
+        :setup_block,
     ],
     format = Documenter.HTML(
         analytics = "UA-90474609-3",
